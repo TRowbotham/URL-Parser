93b2b26cd53df32cb51be8ec5b1394fed7c9202c
Trevor Rowbotham <smokey101stair@gmail.com>
2017-12-06 11:07:18 -0500

    Remove unused method

--- src/URLParser.php
+++ src/URLParser.php
@@ -963,28 +963,4 @@ abstract class URLParser
 
         return $url;
     }
-
-    public static function parseUrl(
-        $input,
-        URLRecord $base = null,
-        $encodingOverride = null
-    ) {
-        $url = self::parseBasicUrl($input, $base, $encodingOverride);
-
-        if ($url === false) {
-            return false;
-        }
-
-        if ($url->scheme !== 'blob') {
-            return $url;
-        }
-
-        // TODO: If the first string in url’s path is not in the blob URL store,
-        // return url
-
-        // TODO: Set url’s object to a structured clone of the entry in the blob
-        // URL store corresponding to the first string in url’s path
-
-        return $url;
-    }
 }
