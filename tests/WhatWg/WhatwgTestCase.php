314bf483d5e708fff31d688d9850bf2eca1b9078
Trevor Rowbotham <trevor.rowbotham@pm.me>
2020-10-09 00:40:56 -0400

    Fix test suite
    New test data added included unpaired surrogates in the json data.
    Unfortunately, PHP's json_decode() has a stroke when it encounters
    unpaired surrogates emitting a JSON_ERROR_UTF16 error code.
    Work around this by pre-replacing unpaired surrogates with a U+FFFD
    replacement character.

--- tests/WhatWg/WhatwgTestCase.php
+++ tests/WhatWg/WhatwgTestCase.php
@@ -9,10 +9,16 @@ use RuntimeException;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 use function GuzzleHttp\Psr7\uri_for;
+use function hexdec;
 use function json_decode;
 use function json_encode;
-use function json_last_error;
 use function json_last_error_msg;
+use function json_last_error;
+use function preg_match;
+use function preg_replace_callback;
+
+use const PREG_OFFSET_CAPTURE;
+use const JSON_ERROR_NONE;
 
 abstract class WhatwgTestCase extends TestCase
 {
@@ -51,7 +57,58 @@ abstract class WhatwgTestCase extends TestCase
             ));
         }
 
-        $json = json_decode((string) $response->getBody(), true);
+        $body = (string) $response->getBody();
+        $matchedPair = false;
+        $filteredJson = preg_replace_callback(
+            '/\\\u([[:xdigit:]]{4})/',
+            static function (array $matches) use ($body, &$matchedPair): string {
+                if ($matchedPair) {
+                    $matchedPair = false;
+
+                    // this was part of the previous matched pair, so leave it untouched
+                    return $matches[0][0];
+                }
+
+                $codePoint1 = hexdec($matches[1][0]);
+
+                if ($codePoint1 >= 0xD800 && $codePoint1 <= 0xDBFF) {
+                    // There is no following code point, so replace it with a U+FFFD
+                    if (
+                        preg_match(
+                            '/\G\\\u([[:xdigit:]]{4})/',
+                            $body,
+                            $m,
+                            PREG_OFFSET_CAPTURE,
+                            $matches[1][1] + 4
+                        ) !== 1
+                    ) {
+                        return "\u{FFFD}";
+                    }
+
+                    $codePoint2 = hexdec($m[1][0]);
+
+                    // If next code point is not a low surrogate, replace it with a U+FFFD
+                    if ($codePoint2 < 0xDC00 || $codePoint2 > 0xDFFF) {
+                        return "\u{FFFD}";
+                    }
+
+                    $matchedPair = true;
+
+                    return $matches[0][0];
+                } elseif ($codePoint1 >= 0xDC00 && $codePoint1 <= 0xDFFF) {
+                    // lone low surrogate, replace it with a U+FFFD
+                    return "\u{FFFD}";
+                }
+
+                // json_decode() can handle surrogate pairs, so leave it untouched
+                return $matches[0][0];
+            },
+            $body,
+            -1,
+            $count,
+            PREG_OFFSET_CAPTURE
+        );
+        $json = json_decode($filteredJson, true);
 
         if (json_last_error() !== JSON_ERROR_NONE) {
             throw new RuntimeException(sprintf(
