86eea43b4289a81901c7ded1f7612618365c8cda
Trevor Rowbotham <trevor.rowbotham@pm.me>
2022-05-15 22:59:44 -0400

    Simplify replacing lone surrogate escape sequences

--- tests/WhatWg/WhatwgTestCase.php
+++ tests/WhatWg/WhatwgTestCase.php
@@ -7,14 +7,10 @@ use PHPUnit\Framework\TestCase;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 use function array_filter;
-use function hexdec;
 use function json_decode;
 use function json_encode;
-use function preg_match;
-use function substr_replace;
 
 use const JSON_THROW_ON_ERROR;
-use const PREG_OFFSET_CAPTURE;
 
 abstract class WhatwgTestCase extends TestCase
 {
@@ -32,47 +28,23 @@ abstract class WhatwgTestCase extends TestCase
                 'base_uri'    => self::WHATWG_BASE_URI,
                 'http_errors' => true,
             ]);
-            $body = (string) $client->get($url)->getBody();
-            $offset = 0;
+            $response = $client->get($url);
 
             // Replace all unpaired surrogate escape sequences with a \uFFFD escape sequence to avoid
             // json_decode() having a stroke and emitting a JSON_ERROR_UTF16 error causing the decode
             // to fail
-            while (preg_match('/\\\u([[:xdigit:]]{4})/', $body, $matches, PREG_OFFSET_CAPTURE, $offset) === 1) {
-                $codePoint1 = hexdec($matches[1][0]);
-                $offset = $matches[0][1] + 6;
+            $body = preg_replace(
+                '/
+                    # Match a low surrogate not proceeded by a high surrogate
+                    (?<!(?<high>\\\u[Dd][89AaBb][[:xdigit:]][[:xdigit:]]))
+                    (?<low>\\\u[Dd][C-Fc-f][[:xdigit:]][[:xdigit:]])
 
-                if ($codePoint1 >= 0xD800 && $codePoint1 <= 0xDBFF) {
-                    // There is no following code point, so replace it with a \uFFFD
-                    if (
-                        preg_match(
-                            '/\G\\\u([[:xdigit:]]{4})/',
-                            $body,
-                            $m,
-                            PREG_OFFSET_CAPTURE,
-                            $matches[1][1] + 4
-                        ) !== 1
-                    ) {
-                        $body = substr_replace($body, '\\uFFFD', $matches[0][1], 6);
-
-                        continue;
-                    }
-
-                    $codePoint2 = hexdec($m[1][0]);
-
-                    // If next code point is not a low surrogate, replace it with a \uFFFD
-                    if ($codePoint2 < 0xDC00 || $codePoint2 > 0xDFFF) {
-                        $body = substr_replace($body, '\\uFFFD', $matches[0][1], 6);
-
-                        continue;
-                    }
-
-                    $offset += 6;
-                } elseif ($codePoint1 >= 0xDC00 && $codePoint1 <= 0xDFFF) {
-                    // lone low surrogate, replace it with a \uFFFD
-                    $body = substr_replace($body, '\\uFFFD', $matches[0][1], 6);
-                }
-            }
+                    # Match a high surrogate not followed by a low surrogate
+                    |(?&high)(?!(?&low))
+                /x',
+                '\uFFFD',
+                (string) $response->getBody()
+            );
 
             // Remove comments and check to make sure it is valid JSON.
             $json = array_filter(json_decode($body, true, self::JSON_DEPTH, JSON_THROW_ON_ERROR), 'is_array');
