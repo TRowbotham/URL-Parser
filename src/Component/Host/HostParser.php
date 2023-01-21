aadd64a462a41e9f010050cacbc1e928518d97b7
Trevor Rowbotham <trevor.rowbotham@pm.me>
2023-01-21 18:45:49 -0500

    Display the ASCII domain in the error as there are too many variables to try and display anything else

--- src/Component/Host/HostParser.php
+++ src/Component/Host/HostParser.php
@@ -75,14 +75,11 @@ class HostParser
             return false;
         }
 
-        if ($asciiDomain->matches('/[' . self::FORBIDDEN_DOMAIN_CODEPOINTS . ']/u')) {
+        if ($asciiDomain->matches('/[' . self::FORBIDDEN_DOMAIN_CODEPOINTS . ']/u', $matches, PREG_OFFSET_CAPTURE)) {
             // Validation error.
-            // Does looking for forbidden code points in the input translate the same as it does in an ascii domain? If not, we will
-            // need to show the ascii domain in the error instead, though it would be preferable to show the original input.
-            $input->matches('/[' . self::FORBIDDEN_DOMAIN_CODEPOINTS . ']/u', $matches, PREG_OFFSET_CAPTURE);
             $context->logger?->warning('domain-forbidden-code-point', [
-                'input'  => (string) $input,
-                'column' => mb_strlen(mb_strcut((string) $input, 0, $matches[0][1], 'utf-8'), 'utf-8') + 1,
+                'input'  => (string) $asciiDomain,
+                'column' => mb_strlen(mb_strcut((string) $asciiDomain, 0, $matches[0][1], 'utf-8'), 'utf-8') + 1,
             ]);
 
             return false;
